46d96cf4d2357c454736b0b206c0f00c73d668ab
Daltonic <darlingtongospel@gmail.com>
2023-12-29 14:03:18 +0100

    deployment script

--- test/DappFundX.test.js
+++ test/DappFundX.test.js
@@ -24,107 +24,107 @@ describe('Contracts', () => {
     await contract.waitForDeployment()
   })
 
-  //   describe('Charity Mgt.', () => {
-  //     beforeEach(async () => {
-  //       await contract
-  //         .connect(owner)
-  //         .createCharity(name, fullname, profile, description, image, toWei(amount))
-  //     })
+    describe('Charity Mgt.', () => {
+      beforeEach(async () => {
+        await contract
+          .connect(owner)
+          .createCharity(name, fullname, profile, description, image, toWei(amount))
+      })
 
-  //     describe('Success', () => {
-  //       it('Should confirm charity creation', async () => {
-  //         result = await contract.getCharities()
-  //         expect(result).to.have.lengthOf(1)
+      describe('Success', () => {
+        it('Should confirm charity creation', async () => {
+          result = await contract.getCharities()
+          expect(result).to.have.lengthOf(1)
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.name).to.be.equal(name)
-  //         expect(result.description).to.be.equal(description)
-  //       })
+          result = await contract.getCharity(id)
+          expect(result.name).to.be.equal(name)
+          expect(result.description).to.be.equal(description)
+        })
 
-  //       it('Should confirm charity update', async () => {
-  //         result = await contract.getCharity(id)
-  //         expect(result.name).to.be.equal(name)
-  //         expect(result.amount).to.be.equal(toWei(amount))
+        it('Should confirm charity update', async () => {
+          result = await contract.getCharity(id)
+          expect(result.name).to.be.equal(name)
+          expect(result.amount).to.be.equal(toWei(amount))
 
-  //         const newName = 'My Updated Charity'
-  //         const newAmount = 2.5
+          const newName = 'My Updated Charity'
+          const newAmount = 2.5
 
-  //         await contract
-  //           .connect(owner)
-  //           .updateCharity(id, newName, fullname, profile, description, image, toWei(newAmount))
+          await contract
+            .connect(owner)
+            .updateCharity(id, newName, fullname, profile, description, image, toWei(newAmount))
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.name).to.be.equal(newName)
-  //         expect(result.amount).to.be.equal(toWei(newAmount))
-  //       })
+          result = await contract.getCharity(id)
+          expect(result.name).to.be.equal(newName)
+          expect(result.amount).to.be.equal(toWei(newAmount))
+        })
 
-  //       it('Should confirm charity deletion', async () => {
-  //         result = await contract.getCharities()
-  //         expect(result).to.have.lengthOf(1)
+        it('Should confirm charity deletion', async () => {
+          result = await contract.getCharities()
+          expect(result).to.have.lengthOf(1)
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.deleted).to.be.equal(false)
+          result = await contract.getCharity(id)
+          expect(result.deleted).to.be.equal(false)
 
-  //         await contract.connect(owner).deleteCharity(id)
+          await contract.connect(owner).deleteCharity(id)
 
-  //         result = await contract.getCharities()
-  //         expect(result).to.have.lengthOf(0)
+          result = await contract.getCharities()
+          expect(result).to.have.lengthOf(0)
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.deleted).to.be.equal(true)
-  //       })
+          result = await contract.getCharity(id)
+          expect(result.deleted).to.be.equal(true)
+        })
 
-  //       it('Should confirm charity banning', async () => {
-  //         result = await contract.getCharities()
-  //         expect(result).to.have.lengthOf(1)
+        it('Should confirm charity banning', async () => {
+          result = await contract.getCharities()
+          expect(result).to.have.lengthOf(1)
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.banned).to.be.equal(false)
+          result = await contract.getCharity(id)
+          expect(result.banned).to.be.equal(false)
 
-  //         await contract.toggleBan(id)
+          await contract.toggleBan(id)
 
-  //         result = await contract.getCharities()
-  //         expect(result).to.have.lengthOf(0)
+          result = await contract.getCharities()
+          expect(result).to.have.lengthOf(0)
 
-  //         result = await contract.getCharity(id)
-  //         expect(result.banned).to.be.equal(true)
-  //       })
-  //     })
+          result = await contract.getCharity(id)
+          expect(result.banned).to.be.equal(true)
+        })
+      })
 
-  //     describe('Failures', () => {
-  //       it('Should confirm charity creation failures', async () => {
-  //         await expect(
-  //           contract
-  //             .connect(owner)
-  //             .createCharity('', fullname, profile, description, image, toWei(amount))
-  //         ).to.be.rejectedWith('Name cannot be empty')
+      describe('Failures', () => {
+        it('Should confirm charity creation failures', async () => {
+          await expect(
+            contract
+              .connect(owner)
+              .createCharity('', fullname, profile, description, image, toWei(amount))
+          ).to.be.rejectedWith('Name cannot be empty')
 
-  //         await expect(
-  //           contract
-  //             .connect(owner)
-  //             .createCharity(name, fullname, profile, description, image, toWei(0))
-  //         ).to.be.rejectedWith('Amount cannot be zero')
-  //       })
+          await expect(
+            contract
+              .connect(owner)
+              .createCharity(name, fullname, profile, description, image, toWei(0))
+          ).to.be.rejectedWith('Amount cannot be zero')
+        })
 
-  //       it('Should confirm charity update failures', async () => {
-  //         await expect(
-  //           contract
-  //             .connect(owner)
-  //             .updateCharity(100, name, fullname, profile, description, image, toWei(amount))
-  //         ).to.be.revertedWith('Charity Not Found')
+        it('Should confirm charity update failures', async () => {
+          await expect(
+            contract
+              .connect(owner)
+              .updateCharity(100, name, fullname, profile, description, image, toWei(amount))
+          ).to.be.revertedWith('Charity Not Found')
 
-  //         await expect(
-  //           contract.updateCharity(id, name, fullname, profile, description, image, toWei(amount))
-  //         ).to.be.revertedWith('Unauthorized Entity')
-  //       })
+          await expect(
+            contract.updateCharity(id, name, fullname, profile, description, image, toWei(amount))
+          ).to.be.revertedWith('Unauthorized Entity')
+        })
 
-  //       it('Should confirm charity banning failures', async () => {
-  //         await expect(contract.connect(owner).toggleBan(id)).to.be.revertedWith(
-  //           'Ownable: caller is not the owner'
-  //         )
-  //       })
-  //     })
-  //   })
+        it('Should confirm charity banning failures', async () => {
+          await expect(contract.connect(owner).toggleBan(id)).to.be.revertedWith(
+            'Ownable: caller is not the owner'
+          )
+        })
+      })
+    })
 
   describe('Donation Mgt.', () => {
     beforeEach(async () => {
